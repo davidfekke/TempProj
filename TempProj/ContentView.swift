02c0ebfd6d52a4f6c90a233fa8affbd124bcfe0a
davidfekke <david@fekke.com>
2019-10-14 19:50:57 -0400

    Initial Commit

---
+++ TempProj/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TempProj
+//
+//  Created by David Fekke on 10/14/19.
+//  Copyright © 2019 David Fekke. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
